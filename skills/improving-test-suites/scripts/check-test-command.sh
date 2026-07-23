d1b9c22b37272e570e721ec49599140d833bf935
Bryan Mendoza <bmendoza.dev@hotmail.com>
2026-07-23 12:49:05 -0600

    skills(improving-test-suites): collapse allowlist arms and allow mix test

--- skills/improving-test-suites/scripts/check-test-command.sh
+++ skills/improving-test-suites/scripts/check-test-command.sh
@@ -23,42 +23,18 @@ case "$cmd_trimmed" in
 esac
 
 case "$cmd_trimmed" in
-pytest\ * | pytest)
-  exit 0
-  ;;
-python\ -m\ pytest\ * | python\ -m\ pytest)
-  exit 0
-  ;;
-go\ test\ * | go\ test)
-  exit 0
-  ;;
-npm\ test\ * | npm\ test)
-  exit 0
-  ;;
-yarn\ test\ * | yarn\ test)
-  exit 0
-  ;;
-pnpm\ test\ * | pnpm\ test)
-  exit 0
-  ;;
-npx\ vitest\ * | npx\ vitest)
-  exit 0
-  ;;
-npx\ jest\ * | npx\ jest)
-  exit 0
-  ;;
-cargo\ test\ * | cargo\ test)
-  exit 0
-  ;;
-mvn\ test\ * | mvn\ test)
-  exit 0
-  ;;
-./gradlew\ test\ * | ./gradlew\ test)
-  exit 0
-  ;;
-rspec\ * | rspec)
-  exit 0
-  ;;
+pytest\ * | pytest | \
+python\ -m\ pytest\ * | python\ -m\ pytest | \
+go\ test\ * | go\ test | \
+npm\ test\ * | npm\ test | \
+yarn\ test\ * | yarn\ test | \
+pnpm\ test\ * | pnpm\ test | \
+npx\ vitest\ * | npx\ vitest | \
+npx\ jest\ * | npx\ jest | \
+cargo\ test\ * | cargo\ test | \
+mvn\ test\ * | mvn\ test | \
+./gradlew\ test\ * | ./gradlew\ test | \
+rspec\ * | rspec | \
 mix\ test\ * | mix\ test)
   exit 0
   ;;
